ee0c5f8886c59f9f2197da2b279885fe31b7d01b
SMarkus27 <pereira.markus.27@hotmail.com>
2024-07-16 18:15:57 -0300

    chore: nginx integration

--- nginx-node-challenge/node/index.js
+++ nginx-node-challenge/node/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 
 const app = express();
 const port = 3000;
@@ -12,39 +12,17 @@ const config = {
 }
 
 
-async function connection() {
-    try {
-        const connection = await mysql.createConnection(config)
-        return connection;
-    }
-    catch (error) {
-        throw error;
-    }
-}
+const conn = mysql.createConnection(config)
+conn.query("CREATE TABLE IF NOT EXISTS people(id INT NOT NULL AUTO_INCREMENT, name VARCHAR(255), PRIMARY KEY(ID));")
+conn.query('INSERT INTO people(name) VALUES("Marcus")')
+conn.end()
 
-async function createTable() {
-    try {
-        const conn = await connection();
-        conn.query("CREATE TABLE IF NOT EXISTS people(id INT NOT NULL AUTO_INCREMENT, name VARCHAR(255), PRIMARY KEY(ID))")
-    }
-    catch (error) {
-        throw error;
-    }
-}
-
-async function createPeople() {
-    try {
-        const conn = await connection();
-        conn.query('INSERT INTO people(name) VALUES("Marcus")')
-    }
-    catch (error) {
-        throw error;
-    }
-}
 
 async function getPeople() {
+    const mysql = require("mysql2/promise");
+
     try {
-        const conn = await connection();
+        const conn = await mysql.createConnection(config);
         const [data] = await conn.query("SELECT name FROM people");
         return data
     }
@@ -52,13 +30,11 @@ async function getPeople() {
         throw error;
     }
 }
+
 app.get("/", async (req, res) => {
-    await createTable();
-    await createPeople();
+
 
     const data = await getPeople();
-    data.map(item => {
-        console.log(item.name)})
     res.send(`<h1>Full Cycle Rocks</h1>
     <ul>${data.map(item => `<li>${item.name}</li>`).join("")}</ul>
 `)
@@ -68,3 +44,9 @@ app.get("/", async (req, res) => {
 app.listen(port, () => {
     console.log("Server started on port: ", port);
 })
+
+
+// connection()
+// createTable()
+// createPeople()
+
